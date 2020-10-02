084ee50de7361db373a89c6ea657537fbd5ac7c1
fathullah auzan <fathullah.auzan@gmail.com>
2020-10-02 18:09:08 +0700

    Initial commit

---
+++ app/src/main/java/id/andan/b201app/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.andan.b201app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/id/andan/b201app/MainMenu.kt
@@ -0,0 +1,11 @@
+package id.andan.b201app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
